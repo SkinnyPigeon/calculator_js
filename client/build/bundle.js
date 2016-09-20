cc96aad30f84a0d5076cbf17490804d1ee0d8819
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-20 22:32:28 +0100

    got the numbers and symbols appearing on screen, need to add the logic in

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -57,18 +57,21 @@
 
 /***/ },
 /* 1 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
+	var Calculator = __webpack_require__( 2 );
+	
 	var CalculatorView = function() {
 	  this.calculator = document.getElementById( 'calculator' );
 	  this.screen = document.createElement( 'h4' );
+	  this.calculator.appendChild( this.screen );
 	}
 	
 	CalculatorView.prototype = {
 	
 	  display: function() {
 	
-	    var buttons = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "%", "+/-", "M+", "MR", "MC" ];
+	    var buttons = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "=", "%", "+/-","C", "M+", "MR", "MC" ];
 	    for( var i = 0; i < buttons.length; i++ ) {
 	      var eachButton = document.createElement( 'button' );
 	      eachButton.innerText = buttons[i];
@@ -76,19 +79,51 @@
 	      this.calculator.appendChild( eachButton );
 	      eachButton.onclick = function(e) {
 	        var id = e.target.id;
-	        console.log( id );
 	        this.handleButtonClick( id );
 	      }.bind( this );
 	    }
 	  },
 	
 	  handleButtonClick: function( id ) {
-	
+	    this.currentScreen = this.screen.innerText
+	    this.screen.innerText = this.currentScreen + id;
+	    this.calculator.appendChild( this.screen );
 	  }
 	
 	}
 	
 	module.exports = CalculatorView;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	var Calculator = function() {
+	  this.result = 0;
+	  this.memory = 0;
+	}
+	
+	Calculator.prototype = {
+	
+	  sum: function( maths ) {
+	    this.result = maths;
+	  },
+	
+	  setMemory: function() {
+	    this.memory = this.result;
+	  },
+	
+	  resetMemory: function() {
+	    this.memory = 0;
+	  },
+	
+	  equals: function() {
+	    return parseFloat( this.result ).toFixed( 8 );
+	  }
+	
+	}
+	
+	module.exports = Calculator;
 
 /***/ }
 /******/ ]);
--- client/src/views/calculator_view.js
+++ client/src/views/calculator_view.js
@@ -1,13 +1,16 @@
+var Calculator = require( '../models/calculator' );
+
 var CalculatorView = function() {
   this.calculator = document.getElementById( 'calculator' );
   this.screen = document.createElement( 'h4' );
+  this.calculator.appendChild( this.screen );
 }
 
 CalculatorView.prototype = {
 
   display: function() {
 
-    var buttons = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "%", "+/-", "M+", "MR", "MC" ];
+    var buttons = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", "=", "%", "+/-","C", "M+", "MR", "MC" ];
     for( var i = 0; i < buttons.length; i++ ) {
       var eachButton = document.createElement( 'button' );
       eachButton.innerText = buttons[i];
@@ -15,14 +18,15 @@ CalculatorView.prototype = {
       this.calculator.appendChild( eachButton );
       eachButton.onclick = function(e) {
         var id = e.target.id;
-        console.log( id );
         this.handleButtonClick( id );
       }.bind( this );
     }
   },
 
   handleButtonClick: function( id ) {
-
+    this.currentScreen = this.screen.innerText
+    this.screen.innerText = this.currentScreen + id;
+    this.calculator.appendChild( this.screen );
   }
 
 }
