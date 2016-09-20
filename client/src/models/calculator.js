e10be622a72340749a2a2f45e48048765b8028e5
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-20 20:22:27 +0100

    about to change the fundamental operation of the way it works

--- client/src/models/calculator.js
+++ client/src/models/calculator.js
@@ -8,6 +8,12 @@ Calculator.prototype = {
     for( var i = 0; i < array.length; i++ ) {
       this.result += array[i];
     }
+  },
+
+  subtract: function( array ) {
+    for( var i = 0; i < array.length; i++ ) {
+      this.result += array[i] - array[ i + 1 ];
+    }
   }
 
 }
