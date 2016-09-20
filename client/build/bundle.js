cbf4e305eb1777a81fef0150e6f79ddc52ad2ce5
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-20 23:10:20 +0100

    basic funcitonality seems to work quite nicely, will add the rest of the features as well as styling tomorrow

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -73,12 +73,11 @@
 	  display: function() {
 	
 	    var numberButtons = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/" ];
-	    var extraFunctionButtons = [ "+/-", "%", "C", "M+", "MR", "MC" ]
-	    var equalsButton = "=";
+	    var extraFunctionButtons = [ "+/-", "%", "M+", "MR", "MC" ]
 	
 	    for( var i = 0; i < numberButtons.length; i++ ) {
 	      var eachNumberButton = document.createElement( 'button' );
-	      eachNumberButton.innerText = numberButtons[i];
+	      eachNumberButton.innerHTML = numberButtons[i];
 	      eachNumberButton.id = numberButtons[i];
 	      this.calculatorSpace.appendChild( eachNumberButton );
 	      eachNumberButton.onclick = function(e) {
@@ -87,12 +86,30 @@
 	      }.bind( this );
 	    }
 	
-	    
+	    var clearButton = document.createElement( 'button' );
+	    clearButton.innerHTML = "C"; 
+	    clearButton.id = "C";
+	    clearButton.onclick = function(e) {
+	
+	    }
+	
+	    var equalsButton = document.createElement( 'button' );
+	    equalsButton.innerHTML = "="; 
+	    equalsButton.id = "=";
+	    this.calculatorSpace.appendChild( equalsButton );
+	    equalsButton.onclick = function(e) {
+	      var question = this.screen.innerHTML;
+	      this.calc.sum( eval(question) );
+	      var answer = this.calc.equals();
+	      this.screen.innerHTML = answer;
+	      this.calculatorSpace.appendChild( this.screen );
+	    }.bind( this );
+	
 	  },
 	
 	  handleNumberButtonClick: function( id ) {
-	    this.currentScreen = this.screen.innerText
-	    this.screen.innerText = this.currentScreen + id;
+	    this.currentScreen = this.screen.innerHTML
+	    this.screen.innerHTML = this.currentScreen + id;
 	    this.calculatorSpace.appendChild( this.screen );
 	  }
 	
