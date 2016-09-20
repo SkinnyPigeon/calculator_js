ab3ae08342864e090f62f06bab635bd3f64ed916
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-20 20:39:33 +0100

    looks like it adds numbers correctly

--- client/src/specs/calculator_spec.js
+++ client/src/specs/calculator_spec.js
@@ -13,10 +13,15 @@ describe( "The Calculator: ", function() {
     assert.equal( 2, calc.result );
   });
 
-  // it( "Should be able to add mulitple numbers", function() {
-  //   calc.add( [ 2, 2, 2 ] );
-  //   assert.equal( 6, calc.result );
-  // });
+  it( "Should be able to add mulitple numbers", function() {
+    calc.setNumber( 2 );
+    calc.sum( "+" );
+    calc.setNumber( 2 );
+    calc.sum( "+" );
+    calc.setNumber( 2 );
+    calc.sum( "+" );
+    assert.equal( 6, calc.result );
+  });
 
   // it( "Should be able to subtract numbers", function() {
   //   calc.subtract( [ 4, 2 ] );
